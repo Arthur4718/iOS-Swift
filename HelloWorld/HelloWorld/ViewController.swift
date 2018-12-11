e5b1f61888a3804d13bfeabc199de9b92cb25de9
Unknown <arthur.gomes_4718@hotmail.com>
2018-12-11 18:53:40 -0200

    HelloWorld

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Arthur on 11/12/18.
+//  Copyright © 2018 devarthur4718. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        let number = 2
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
