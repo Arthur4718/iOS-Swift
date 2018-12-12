2d6f1013f24b743c0a98ced4828b6abad262bc0e
Unknown <arthur.gomes_4718@hotmail.com>
2018-12-12 14:28:21 -0200

    Xcode - Data types, var e let Keywords

--- Xcode PlayGround/Vars,Constants and Data Types..playground/Contents.swift
+++ Xcode PlayGround/Vars,Constants and Data Types..playground/Contents.swift
@@ -2,4 +2,33 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var powerLevel : Int = 8001
+
+powerLevel = 9000
+
+print(powerLevel)
+
+//Read only variable use let keyword
+
+let fighterName : String = "Kakarot"
+let secondFigher : String = "Vegeta"
+//Strings
+let fullText : String = fighterName + " and " +  secondFigher
+//Strings concatenation
+let myFirstName : String = "Arthur"
+let mySecondName : String = "Gomes"
+var myAge : Int = 25
+let myFullName = "\(myFirstName) \(mySecondName) "
+
+//Using diferent data types
+let myDetails : String = "\(myFirstName), \(myAge)"
+
+//other data types
+let booleans : Bool = true
+
+let floatNumber : Float = 13.6
+
+//For aditional precision use Double
+let doubleNumber : Double = 3.14531234512
+
+
