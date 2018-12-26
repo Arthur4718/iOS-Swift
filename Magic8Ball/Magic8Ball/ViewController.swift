3974c0c24b72acdc15f902bb9cda783f1b9c72e3
Unknown <arthur.gomes_4718@hotmail.com>
2018-12-26 21:24:00 -0200

    Random sort

--- Magic8Ball/Magic8Ball/ViewController.swift
+++ Magic8Ball/Magic8Ball/ViewController.swift
@@ -8,11 +8,21 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    let imageArray = ["ball1","ball2","ball3","ball4","ball5"]
+    
+    var randomBallNumber : Int = 0
+    
+    @IBOutlet weak var textResponse: UILabel!
+    @IBOutlet weak var imgBall: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        randomBallNumber = Int(arc4random_uniform(4))
+        imgBall.image = UIImage(named: imageArray[randomBallNumber])
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +30,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func showText(_ sender: Any) {
+        
+    }
+    
 }
 
